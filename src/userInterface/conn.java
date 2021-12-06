0b325827ada7102ad331a42b810ccfda73202a2d
gamingflexer <osurve20ece@student.mes.ac.in>
2021-12-06 16:51:17 +0530

    x

---
+++ src/userInterface/conn.java
@@ -0,0 +1,20 @@
+package userInterface;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///hms","root",""); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
